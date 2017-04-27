fe19207a2d8e10a3ab90b45044a3badc2847d488
AAKASH HANDA <AAKASH HANDA>
2017-04-28 00:28:17 +0530

    a

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var app = express();
-var port=process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
+var nav = [{
+      Links: '/Books',
+      Text: 'Book'
+    }, {
+      Links: '/Authors',
+      Text: 'Author'
+    }]
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static(__dirname + '/public'));
-app.set('views','./src/views');
-app.set('view engine','ejs');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 //app.use(express.static(__dirname + '/src/views'));
 
-app.get('/',function(req,res){
-	res.render('index',{title:"hello i am here",list:['a','b']})
+
+app.use('/Books', bookRouter);
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: "hello i am here",
+    nav: nav
+  });
 });
 
-app.get('/books',function(req,res){
-	res.send('books')
+app.get('/books', function(req, res) {
+  res.send('books')
 });
 
-app.listen(port,function(err){
-	console.log('running');
+app.listen(port, function(err) {
+  console.log('running');
 })
